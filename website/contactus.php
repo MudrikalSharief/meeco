8dafe9e0634b082180f0d524bd8669dae6dd962b
AlmcmhirTech <almcmhirservices@gmail.com>
2024-11-29 14:04:58 +0800

    Add files via upload

---
+++ website/contactus.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Summarizer and Reviewer</title>
+    <link rel="stylesheet" href="css/landing-page-style.css?v=<?php echo time(); ?>">
+    <link rel="stylesheet" href="css/navbar-style.css?v=<?php echo time(); ?>">
+    <link rel="stylesheet" href="css/footer-style.css?v=<?php echo time(); ?>">
+</head>
+<body>
+<nav class="navbar">
+        <div class="container">
+            <div class="logo">
+            <a href="landing_page.php" style="text-decoration: none;"><img src="pictures/meeco-logo-text.png" alt="Meeco Logo"></a>
+            </div>
+            <button class="login-btn">Log In</button>
+        </div>
+    </nav>
+    <main>
+    <section class="how-it-works">
+            <div class="container">
+                <h2 class="text-how-it-works"><img src="pictures/sparkling.png" alt="sparkle" class="sparkle"></span>Contact <span class="highlight-blue">Us</span><img src="pictures/sparkling.png" alt="sparkle" class="sparkle"></span></h2>
+                <p class="section-desc" style="font-size: 20px; font-weight:500; margin-bottom: 32px;">Email: meeco@gmail.com</p>
+                <p class="section-desc" style="font-size: 20px; font-weight:500; margin-bottom: 32px;">Phone: (+63) 95382938392</p>
+            </div>
+        </section>
